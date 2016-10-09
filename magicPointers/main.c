022059e806a40cc6ba848ad11158fc9161d1d5f7
youdy3000 <robert.youdale@gmail.com>
2016-10-09 13:59:15 +0100

    Initial Commit

---
+++ magicPointers/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  magicPointers
+//
+//  Created by Robert Youdale on 09/10/2016.
+//  Copyright © 2016 youdog. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
