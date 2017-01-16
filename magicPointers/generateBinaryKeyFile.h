1eb0f79a6c6033dd3c155607ca2a7d7dc084fffe
youdy3000 <robert.youdale@gmail.com>
2017-01-16 13:26:33 +0000

    re-factored code to hide static functions

--- magicPointers/generateBinaryKeyFile.h
+++
@@ -1,8 +0,0 @@
-#ifndef generateBinaryKeyFile_h
-#define generateBinaryKeyFile_h
-
-#include <stdio.h>
-
-void *generate_bin_key_file(unsigned char *derived_secret);
-
-#endif /* generateBinaryKeyFile_h */
